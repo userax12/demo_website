67835331fcd5da1e17e94f249594c0f56108e15c
user <user@company.com>
2023-12-17 11:03:45 +0800

    modify styles

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,16 @@
 window.index = [{
-    "title": "article 1",
+    "title": "article one",
     "route": "data/1.txt"
 },{
-    "title": "article 2",
+    "title": "article two",
     "route": "data/2.txt"
+},{
+    "title": "article three",
+    "route": "data/3.txt"
+},{
+    "title": "article four",
+    "route": "data/4.txt"
+},{
+    "title": "article five",
+    "route": "data/5.txt"
 }];
